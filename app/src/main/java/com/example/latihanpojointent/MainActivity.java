605abb368541204161055ea4bf48d4d7eeb718fa
HaikalPWPB <haikalg2003@gmail.com>
2020-08-24 15:37:18 +0700

    Fixing bugs

--- app/src/main/java/com/example/latihanpojointent/MainActivity.java
+++ app/src/main/java/com/example/latihanpojointent/MainActivity.java
@@ -22,11 +22,9 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         btnMoveActivity = (Button)findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
-        btnMoveWithDataActivity =
-                (Button)findViewById(R.id.btn_move_with_data_activity);
+        btnMoveWithDataActivity = (Button)findViewById(R.id.btn_move_with_data_activity);
         btnMoveWithDataActivity.setOnClickListener(this);
-        btnMoveWithObject =
-                (Button)findViewById(R.id.btn_move_activity_object);
+        btnMoveWithObject = (Button)findViewById(R.id.btn_move_activity_object);
         btnMoveWithObject.setOnClickListener(this);
         btnDialNumber = findViewById(R.id.btn_dial_number);
         btnDialNumber.setOnClickListener(this);
@@ -38,62 +36,42 @@ public class MainActivity extends AppCompatActivity implements
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_move_activity:
-                Intent moveIntent = new Intent(MainActivity.this,
-
-                        MoveActivity.class);
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
                 startActivity(moveIntent);
-
                 break;
-
             case R.id.btn_move_with_data_activity:
-                Intent moveWithDataIntent = new Intent(MainActivity.this,
-                        MoveWithDataActivity.class);
-                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME,
-                        "Haikal Putra Gustiansyah");
-                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,
-                        24);
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Haikal Putra Gustiansyah");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 17);
                 startActivity(moveWithDataIntent);
-
                 break;
-
             case R.id.btn_move_activity_object:
                 Person mPerson = new Person();
                 mPerson.setName("Haikal Putra Gustiansyah");
                 mPerson.setAge(17);
-
                 mPerson.setEmail("haikalg2003@gmail.com");
-
                 mPerson.setCity("Bandung");
-                Intent moveWithObjectIntent = new Intent(MainActivity.this,
-                        MoveWithObjectActivity.class);
+                Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, mPerson);
                 startActivity(moveWithObjectIntent);
-
                 break;
-
             case R.id.btn_dial_number:
                 String phoneNumber = "089656004907";
-                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL,
-                        Uri.parse("telephone:" + phoneNumber));
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
                 startActivity(dialPhoneIntent);
-
                 break;
-
             case R.id.btn_move_for_result:
-                Intent moveForResultIntent = new Intent(MainActivity.this,
-                        MoveForResultActivity.class);
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
                 startActivityForResult(moveForResultIntent, REQUEST_CODE);
                 break;
         }
     }
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent
-            data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE) {
             if (resultCode == MoveForResultActivity.RESULT_CODE) {
-                int selectedValue =
-                        data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
                 tvResult.setText(String.format("Hasil : %s", selectedValue));
             }
         }
